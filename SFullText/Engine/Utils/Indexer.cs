5f546648ee7c28e3f34929766136de862c52493e
hoodd <sidorov-i-o@mail.ru>
2022-10-07 12:59:41 +0500

    New NgrammCreate

--- SFullText/Engine/Utils/Indexer.cs
+++ SFullText/Engine/Utils/Indexer.cs
@@ -5,6 +5,14 @@ namespace SFullText.Engine.Utils
 {
     public static class Indexer
     {
+        public static IndexStorage<T> CreateNgrammIndex<T>(IReadOnlyDictionary<int, T> storgae, int ngrammLenght = 3) where T : ISearchModel
+        {
+            var index = new NGrammIndex(ngrammLenght);
+            index.Create(storgae!.Values.Select(sp => (ISearchModel)sp));
+
+            return new(storgae, index);
+        }
+
         public static IndexStorage<T> CreateNgrammIndex<T>(IEnumerable<T> searchModels, int ngrammLenght = 3) where T : ISearchModel
         {
             var storgae = new Dictionary<int, T>();
